97dbf1001b485bb8393500e670d1bebee71acae8
Ben Fleuty <22148138+benfleuty@users.noreply.github.com>
2020-11-26 11:40:31 +0000

    Added a base page
    Pages can access common attributes

---
+++ CMP 205 Unit 1 WPF Application for Comic Book Store scenario/DundeeComicBookStore/Pages/BasePage.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace DundeeComicBookStore.Pages
+{
+    public abstract class BasePage : Page
+    {
+        protected Window _currentWindow;
+
+        protected Window CurrentWindow
+        {
+            get { return _currentWindow; }
+            set { _currentWindow = value; }
+        }
+
+        public BasePage()
+        {
+            CurrentWindow = App.Current.MainWindow;
+        }
+    }
+}
