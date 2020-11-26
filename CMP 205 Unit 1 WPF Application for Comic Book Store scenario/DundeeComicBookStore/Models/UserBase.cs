23805ca65d6959fe6c1364c5a9bd629689cc170c
Ben Fleuty <22148138+benfleuty@users.noreply.github.com>
2020-11-26 13:57:50 +0000

    Added a base user class
    User and staff will inherit from this

---
+++ CMP 205 Unit 1 WPF Application for Comic Book Store scenario/DundeeComicBookStore/Models/UserBase.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DundeeComicBookStore.Models
+{
+    public abstract class UserBase
+    {
+        private int _id;
+
+        public int ID
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+    }
+}
