a86e08c963c5b0759c8ae671448bf2e93d4e9b61
Ben Fleuty <22148138+benfleuty@users.noreply.github.com>
2020-11-29 17:47:05 +0000

    Switched to interface from inheritance

---
+++ CMP 205 Unit 1 WPF Application for Comic Book Store scenario/DundeeComicBookStore/Interfaces/UserInterface.cs
@@ -0,0 +1,19 @@
+﻿using DundeeComicBookStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DundeeComicBookStore.Interfaces
+{
+    internal interface IUser
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string EmailAddress { get; set; }
+        public AddressModel Address { get; set; }
+        public string ProfilePictureSource { get; set; }
+        public uint RewardPoints { get; set; }
+    }
+}
