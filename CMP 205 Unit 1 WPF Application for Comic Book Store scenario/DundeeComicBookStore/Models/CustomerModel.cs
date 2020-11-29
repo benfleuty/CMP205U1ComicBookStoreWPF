8da5ddd35f00614e9116d89cad4f96d8d90ca10e
Ben Fleuty <22148138+benfleuty@users.noreply.github.com>
2020-11-29 17:49:05 +0000

    Implement IUser interface

--- CMP 205 Unit 1 WPF Application for Comic Book Store scenario/DundeeComicBookStore/Models/CustomerModel.cs
+++ CMP 205 Unit 1 WPF Application for Comic Book Store scenario/DundeeComicBookStore/Models/CustomerModel.cs
@@ -1,10 +1,19 @@
-﻿using System;
+﻿using DundeeComicBookStore.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace DundeeComicBookStore.Models
 {
-    public class CustomerModel
+    public class CustomerModel : IUser
     {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string EmailAddress { get; set; }
+        public AddressModel Address { get; set; }
+        public string ProfilePictureSource { get; set; }
+        public uint RewardPoints { get; set; }
     }
 }
