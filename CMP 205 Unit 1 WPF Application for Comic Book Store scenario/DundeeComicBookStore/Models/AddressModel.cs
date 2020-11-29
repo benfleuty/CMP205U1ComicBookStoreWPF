27e1286c9656e5f338d4fafb5a05a19a53e7ce84
Ben Fleuty <22148138+benfleuty@users.noreply.github.com>
2020-11-29 17:55:00 +0000

    Added attributes

--- CMP 205 Unit 1 WPF Application for Comic Book Store scenario/DundeeComicBookStore/Models/AddressModel.cs
+++ CMP 205 Unit 1 WPF Application for Comic Book Store scenario/DundeeComicBookStore/Models/AddressModel.cs
@@ -1,10 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace DundeeComicBookStore.Models
+﻿namespace DundeeComicBookStore.Models
 {
     public class AddressModel
     {
+        private string _houseNameOrNumber;
+
+        public string HouseNameOrNumber
+        {
+            get { return _houseNameOrNumber; }
+            set { _houseNameOrNumber = value; }
+        }
+
+        private string _nameOfRoad;
+
+        public string NameOfRoad
+        {
+            get { return _nameOfRoad; }
+            set { _nameOfRoad = value; }
+        }
+
+        private string _postcode;
+
+        public string Postcode
+        {
+            get { return _postcode; }
+            set { _postcode = value; }
+        }
     }
 }
