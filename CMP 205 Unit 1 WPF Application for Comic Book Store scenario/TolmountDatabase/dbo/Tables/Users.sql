a043536e6139d84de92f3e463c4de8269565c5be
Ben Fleuty <22148138+benfleuty@users.noreply.github.com>
2020-11-29 18:32:04 +0000

    fixed the db i think

--- CMP 205 Unit 1 WPF Application for Comic Book Store scenario/TolmountDatabase/dbo/Tables/Users.sql
+++ CMP 205 Unit 1 WPF Application for Comic Book Store scenario/TolmountDatabase/dbo/Tables/Users.sql
@@ -1,7 +1,7 @@
 ﻿CREATE TABLE [dbo].[Users] (
     [id]             INT           IDENTITY (1, 1) NOT NULL,
-    [fname]          VARCHAR (50)  NOT NULL,
-    [lname]          VARCHAR (50)  NOT NULL,
+    [firstName]          VARCHAR (50)  NOT NULL,
+    [lastName]          VARCHAR (50)  NOT NULL,
     [phone]          VARCHAR (11)  NULL,
     [email]          VARCHAR (254) NOT NULL,
     [address]        TEXT          NULL,
