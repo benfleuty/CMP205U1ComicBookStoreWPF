f8e80bb7123dcc8c7847e8e4a5808d33b02ef8dc
Ben Fleuty <22148138+benfleuty@users.noreply.github.com>
2020-12-02 17:50:11 +0000

    Multiple changes

--- CMP 205 Unit 1 WPF Application for Comic Book Store scenario/DundeeComicBookStore/Helpers/InputValidationHelper.cs
+++ CMP 205 Unit 1 WPF Application for Comic Book Store scenario/DundeeComicBookStore/Helpers/InputValidationHelper.cs
@@ -1,84 +1,143 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
 
 namespace DundeeComicBookStore.Helpers
 {
     public class InputValidationHelper
     {
-        public static bool ValidateEmail(string email)
+        private static bool ValidPhoneNumber(string phone)
+        {
+            int digitCount = 0;
+            foreach (char c in phone)
+            {
+                if (char.IsDigit(c))
+                    ++digitCount;
+                else if (char.IsWhiteSpace(c)) continue;
+                else return false;
+            }
+
+            if (digitCount != 11) return false;
+            return true;
+        }
+
+        private static void SanitisePhoneNumber(ref string number)
+        {
+            string result = string.Empty;
+            foreach (char c in number)
+            {
+                // if number
+                if (char.IsDigit(c)) result += c;
+                // if space after a number
+                else if (char.IsWhiteSpace(c) && char.IsDigit(result[^1]))
+                    result += c;
+            }
+            number = result;
+        }
+
+        private static bool ValidEmail(string email)
         {
             var emailChecker = new System.ComponentModel.DataAnnotations.EmailAddressAttribute();
             return emailChecker.IsValid(email);
         }
 
-        public static bool ValidateNewPassword(string password)
+        private static bool ValidNewPassword(string password)
         {
             return password.Length > 0;
         }
 
-        public static bool ValidatePasswords(string password1, string password2)
+        private static bool ValidPasswords(string password1, string password2)
         {
             return password1 == password2;
         }
 
-        public static bool ValidateText(string text)
+        private static bool ValidText(string text)
         {
             return text.Length > 0;
         }
 
-        public static void ValidateInput(object sender, object compare, TextBlock errorMsg, ErrorHelper.UIError uiError)
-        {
-            switch (uiError)
-            {
-                case ErrorHelper.UIError.PasswordMismatch:
-                    string password1 = ((PasswordBox)sender).Password;
-                    string password2 = ((PasswordBox)compare).Password;
-                    if (InputValidationHelper.ValidatePasswords(password1, password2))
-                        ErrorHelper.ClearInputError(errorMsg);
-                    else ErrorHelper.ShowInputError(uiError, errorMsg);
-                    break;
-            }
-        }
-
-        public static void ValidateInput(object sender, TextBlock errorMsgTextBlock, ErrorHelper.UIError uiErrorReason)
+        public static bool ValidInput(object sender, TextBlock errorMsgTextBlock, ErrorHelper.UIError uiErrorReason)
         {
             switch (uiErrorReason)
             {
                 case ErrorHelper.UIError.RequiredField:
                     string text = ((TextBox)sender).Text;
                     // If the text is valid
-                    if (InputValidationHelper.ValidateText(text))
+                    if (ValidText(text))
+                    {
                         ErrorHelper.ClearInputError(errorMsgTextBlock);
+                        return true;
+                    }
                     // Text is not valid
-                    else ErrorHelper.ShowInputError(uiErrorReason, errorMsgTextBlock);
+                    break;
+
+                case ErrorHelper.UIError.InvalidPhoneNumber:
+                    string number = ((TextBox)sender).Text;
+                    if (ValidPhoneNumber(number))
+                    {
+                        SanitisePhoneNumber(ref number);
+                        ((TextBox)sender).Text = number;
+                        ErrorHelper.ClearInputError(errorMsgTextBlock);
+                        return true;
+                    }
                     break;
 
                 case ErrorHelper.UIError.InvalidEmail:
-                    // Check if email is valid
-                    string email = ((TextBox)sender).Text;
-                    if (InputValidationHelper.ValidateEmail(email))
-                        ErrorHelper.ClearInputError(errorMsgTextBlock);
-                    else ErrorHelper.ShowInputError(uiErrorReason, errorMsgTextBlock);
-                    break;
+                    {
+                        // Check if email is valid
+                        string email = ((TextBox)sender).Text;
+                        if (ValidEmail(email))
+                        {
+                            ErrorHelper.ClearInputError(errorMsgTextBlock);
+                            return true;
+                        }
+                        break;
+                    }
 
-                case ErrorHelper.UIError.UsedEmail:
-                    throw new NotImplementedException();
-                //break;
+                case ErrorHelper.UIError.EmailInUse:
+                    {
+                        string email = ((TextBox)sender).Text;
+                        if (DBAccessHelper.IsEmailNotInUse(email))
+                        {
+                            ErrorHelper.ClearInputError(errorMsgTextBlock);
+                            return true;
+                        }
+                        break;
+                    }
 
                 case ErrorHelper.UIError.PasswordComplexity:
-                    string password = ((PasswordBox)sender).ToString();
-                    if (InputValidationHelper.ValidateNewPassword(password))
+                    string password = ((PasswordBox)sender).Password.ToString();
+                    if (ValidNewPassword(password))
+                    {
                         ErrorHelper.ClearInputError(errorMsgTextBlock);
-                    else ErrorHelper.ShowInputError(uiErrorReason, errorMsgTextBlock);
+                        return true;
+                    }
                     break;
 
                 default:
                     MessageBox.Show("The given error message is not handled here!", "Error RegisterPage.xaml.cs:ValidateInput(3)", MessageBoxButton.OK, MessageBoxImage.Error);
                     break;
             }
+            ErrorHelper.ShowInputError(uiErrorReason, errorMsgTextBlock);
+            return false;
+        }
+
+        public static bool ValidInput(object sender, object compare, TextBlock errorMsg, ErrorHelper.UIError uiError)
+        {
+            switch (uiError)
+            {
+                case ErrorHelper.UIError.PasswordMismatch:
+                    string password1 = ((PasswordBox)sender).Password;
+                    string password2 = ((PasswordBox)compare).Password;
+                    if (InputValidationHelper.ValidPasswords(password1, password2))
+                    {
+                        ErrorHelper.ClearInputError(errorMsg);
+                        return true;
+                    }
+                    else ErrorHelper.ShowInputError(uiError, errorMsg);
+                    break;
+            }
+
+            return false;
         }
     }
 }
