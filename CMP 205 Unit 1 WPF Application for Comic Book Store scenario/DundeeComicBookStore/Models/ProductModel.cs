9727767e3403de15a9a0caf71594ebdd5fc2811c
Ben Fleuty <22148138+benfleuty@users.noreply.github.com>
2020-12-05 16:59:45 +0000

    Added Product Model

---
+++ CMP 205 Unit 1 WPF Application for Comic Book Store scenario/DundeeComicBookStore/Models/ProductModel.cs
@@ -0,0 +1,16 @@
+﻿using DundeeComicBookStore.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DundeeComicBookStore.Models
+{
+    public class ProductModel : IProduct
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public uint UnitPrice { get; set; }
+        public uint UnitsInStock { get; set; }
+        public uint UnitCost { get; set; }
+    }
+}
