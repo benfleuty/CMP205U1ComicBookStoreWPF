344e8eed336232b5f6484158833f7f06e71259a6
Ben Fleuty <22148138+benfleuty@users.noreply.github.com>
2020-12-05 18:09:23 +0000

    Change money props to decimal from int

--- CMP 205 Unit 1 WPF Application for Comic Book Store scenario/DundeeComicBookStore/Interfaces/ProductInterface.cs
+++ CMP 205 Unit 1 WPF Application for Comic Book Store scenario/DundeeComicBookStore/Interfaces/ProductInterface.cs
@@ -8,8 +8,8 @@ namespace DundeeComicBookStore.Interfaces
     {
         public string Name { get; set; }
         public string Description { get; set; }
-        public uint UnitPrice { get; set; }
+        public decimal UnitPrice { get; set; }
         public uint UnitsInStock { get; set; }
-        public uint UnitCost { get; set; }
+        public decimal UnitCost { get; set; }
     }
 }
--- CMP 205 Unit 1 WPF Application for Comic Book Store scenario/DundeeComicBookStore/Models/ProductModel.cs
+++ CMP 205 Unit 1 WPF Application for Comic Book Store scenario/DundeeComicBookStore/Models/ProductModel.cs
@@ -9,8 +9,8 @@ namespace DundeeComicBookStore.Models
     {
         public string Name { get; set; }
         public string Description { get; set; }
-        public uint UnitPrice { get; set; }
+        public decimal UnitPrice { get; set; }
         public uint UnitsInStock { get; set; }
-        public uint UnitCost { get; set; }
+        public decimal UnitCost { get; set; }
     }
 }
