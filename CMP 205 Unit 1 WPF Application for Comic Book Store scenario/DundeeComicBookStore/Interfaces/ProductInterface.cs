06d51eebe138f6d80fc9f72ca570df1b4cdc0025
Ben Fleuty <22148138+benfleuty@users.noreply.github.com>
2020-12-05 16:55:39 +0000

    Created Product Interface

---
+++ CMP 205 Unit 1 WPF Application for Comic Book Store scenario/DundeeComicBookStore/Interfaces/ProductInterface.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DundeeComicBookStore.Interfaces
+{
+    public interface IProduct
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public uint UnitPrice { get; set; }
+        public uint UnitsInStock { get; set; }
+        public uint UnitCost { get; set; }
+    }
+}
