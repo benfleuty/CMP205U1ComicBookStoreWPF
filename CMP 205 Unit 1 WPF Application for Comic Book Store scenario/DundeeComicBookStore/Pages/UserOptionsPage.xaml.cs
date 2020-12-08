408cf34eb5f2ede65412ce1e19ae27b857d34ec0
Ben Fleuty <22148138+benfleuty@users.noreply.github.com>
2020-12-08 13:21:58 +0000

    Get search results

--- CMP 205 Unit 1 WPF Application for Comic Book Store scenario/DundeeComicBookStore/Pages/UserOptionsPage.xaml.cs
+++ CMP 205 Unit 1 WPF Application for Comic Book Store scenario/DundeeComicBookStore/Pages/UserOptionsPage.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using DundeeComicBookStore.Interfaces;
 using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -21,6 +22,7 @@ namespace DundeeComicBookStore.Pages
     public partial class UserOptionsPage : BasePage
     {
         private decimal highestCost = 0.00m;
+        private List<IProduct> currentSearchResults;
         private IUser _user;
 
         public IUser User
@@ -32,11 +34,8 @@ namespace DundeeComicBookStore.Pages
             User = loggedInUser;
 
             DisplayUserInfo();
-            List<IProduct> catalog = GetCatalog();
 
-            if (!CheckCatalog(catalog)) return;
-
-            ShowCatalog(catalog);
+            OutputSearchResults(DBAccessHelper.GetAllProducts());
         }
 
         private void DisplayUserInfo()
@@ -52,6 +51,115 @@ namespace DundeeComicBookStore.Pages
             return products;
         }
 
+        private List<IProduct> GetCatalog(string search, bool getAll = false)
+        {
+            string select = "SELECT name,description,unitPrice,stockCount";
+            string from = "FROM Products";
+            string where = "WHERE";
+            string and = "AND ";
+            string or = "OR ";
+            string orderby = "ORDER BY";
+
+            if (titleCheckBox.IsChecked == true && getAll == false)
+            {
+                if (descriptionCheckBox.IsChecked == false)
+                    where = $"{where} name LIKE @title";
+                if (descriptionCheckBox.IsChecked == true)
+                    where = $"{where} ( name LIKE @title";
+            }
+
+            if (descriptionCheckBox.IsChecked == true && getAll == false)
+            {
+                if (where != "WHERE")
+                    where = $"{where} {or}";
+
+                if (where != "WHERE")
+                    where = $"{where} description LIKE @description)";
+                else where = $"{where} description LIKE @description";
+            }
+
+            if (priceRangeCheckBox.IsChecked == true)
+            {
+                if (minimumPriceDUD.Value != null && minimumPriceDUD.Value > 0)
+                {
+                    if (where != "WHERE")
+                        where = $"{where} {and}";
+
+                    where = $"{where} unitPrice >= @minPrice";
+                }
+
+                if (maximumPriceDUD.Value != null)
+                {
+                    if (where != "WHERE")
+                        where = $"{where} {and}";
+                    where = $"{where} unitPrice <= @maxPrice";
+                }
+            }
+
+            var selectedItem = (ComboBoxItem)sortByComboBox.SelectedItem;
+            string tag = selectedItem.Tag.ToString();
+            if (tag == "noSort")
+                orderby = string.Empty;
+            else
+                orderby = $"{orderby} {tag}";
+
+            if (where == "WHERE") where = string.Empty;
+
+            string query = $"{select} {from} {where} {orderby}";
+
+            var parameterKVPs = new Dictionary<string, object>();
+
+            if (titleCheckBox.IsChecked == true)
+                parameterKVPs.Add("@title", $"%{search}%");
+
+            if (descriptionCheckBox.IsChecked == true)
+                parameterKVPs.Add("@description", $"%{search}%");
+
+            if (priceRangeCheckBox.IsChecked == true)
+            {
+                if (minimumPriceDUD.Value != null && minimumPriceDUD.Value > 0)
+                    parameterKVPs.Add("@minPrice", (decimal)minimumPriceDUD.Value);
+
+                if (maximumPriceDUD.Value != null)
+                    parameterKVPs.Add("@maxPrice", (decimal)maximumPriceDUD.Value);
+            }
+
+            List<IProduct> products = DBAccessHelper.GetProducts(query, parameterKVPs);
+
+            return products;
+        }
+
+        private void LogoutButton_Click(object sender, RoutedEventArgs e)
+        {
+            ChangePageTo(new LoginPage());
+        }
+
+        private void ClearButton_Click(object sender, RoutedEventArgs e)
+        {
+            searchTextbox.Text = string.Empty;
+        }
+
+        private void PriceRangeCheckBox_CheckedChanged(object sender, RoutedEventArgs e)
+        {
+            priceRangeContainer.IsEnabled = (bool)priceRangeCheckBox.IsChecked;
+        }
+
+        private void SearchButton_Click(object sender, RoutedEventArgs e)
+        {
+            string search = searchTextbox.Text;
+            if (search == string.Empty)
+                OutputSearchResults(GetCatalog(search, true));
+            else
+                OutputSearchResults(GetCatalog(search));
+        }
+
+        private void OutputSearchResults(List<IProduct> results)
+        {
+            if (!CheckCatalog(results)) return;
+
+            ShowCatalog(results);
+        }
+
         private bool CheckCatalog(List<IProduct> catalog)
         {
             // no products
@@ -61,6 +169,7 @@ namespace DundeeComicBookStore.Pages
                 {
                     Text = "There are no products to display at this time!"
                 };
+                resultsViewer.Children.Clear();
                 resultsViewer.Children.Add(noProducts);
                 return false;
             }
@@ -69,14 +178,29 @@ namespace DundeeComicBookStore.Pages
 
         private void ShowCatalog(List<IProduct> catalog)
         {
+            resultsViewer.Children.Clear();
             // products
             foreach (IProduct product in catalog)
             {
                 string name = product.Name;
-                int descLength = (product.Description.Length > 64) ? 64 : product.Description.Length;
-                string desciption = $"{product.Description.Substring(0, descLength)}...";
+                int descLength = (product.Description.Length >= 61) ? 61 : product.Description.Length;
+                bool cutOffDescription = descLength == 61;
+                string desciption =
+                    (cutOffDescription)
+                    ? $"{product.Description.Substring(0, descLength)}..."
+                    : product.Description;
                 string price = product.UnitPrice.ToString("C");
-                string text = $"{name}\n{desciption}\n{price}";
+                bool anyInStock = product.UnitsInStock > 1;
+                bool lowStock = product.UnitsInStock < 10;
+                string inStock = string.Empty;
+                if (anyInStock)
+                {
+                    if (lowStock) inStock += "LOW STOCK: ";
+                    inStock += $"{product.UnitsInStock} available";
+                }
+                else inStock = "OUT OF STOCK!";
+
+                string text = $"{name}\n\n{desciption}\n\n{price}\n\n{inStock}";
                 var content = new TextBlock()
                 {
                     Text = text,
@@ -91,25 +215,6 @@ namespace DundeeComicBookStore.Pages
                 if (highestCost < product.UnitPrice)
                     highestCost = product.UnitPrice;
             }
-        }
-
-        private void LogoutButton_Click(object sender, RoutedEventArgs e)
-        {
-            ChangePageTo(new LoginPage());
-        }
-
-        private void ClearButton_Click(object sender, RoutedEventArgs e)
-        {
-            searchTextbox.Text = string.Empty;
-        }
-
-        private void filterResultsButton_Click(object sender, RoutedEventArgs e)
-        {
-        }
-
-        private void priceRangeCheckBox_CheckedChanged(object sender, RoutedEventArgs e)
-        {
-            priceRangeContainer.IsEnabled = (bool)priceRangeCheckBox.IsChecked;
         }
     }
 }
