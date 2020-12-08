4a87a6ecffb46005ae59859877585910c95c616e
Ben Fleuty <22148138+benfleuty@users.noreply.github.com>
2020-12-08 18:17:45 +0000

    add product viewer

---
+++ CMP 205 Unit 1 WPF Application for Comic Book Store scenario/DundeeComicBookStore/Windows/ProductViewerWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using DundeeComicBookStore.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace DundeeComicBookStore.Windows
+{
+    /// <summary>
+    /// Interaction logic for ProductViewer.xaml
+    /// </summary>
+    public partial class ProductViewerWindow : Window
+    {
+        public ProductViewerWindow(IProduct product)
+        {
+            InitializeComponent();
+
+            productTitleTextblock.Text = product.Name;
+            productDesciptionTextblock.Text = product.Description;
+            productStockCountTextblock.Text = product.UnitsInStock.ToString();
+            quantityToAdd.Maximum = (int)product.UnitsInStock;
+            quantityToAdd.Minimum = 1;
+        }
+
+        private void closeProductViewerButton_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
