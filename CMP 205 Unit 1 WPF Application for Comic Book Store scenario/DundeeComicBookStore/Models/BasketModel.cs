a9d4bbb7ab20afa0369f65accba562bfdd33d47d
Ben Fleuty <22148138+benfleuty@users.noreply.github.com>
2020-12-10 13:45:46 +0000

    multiple changes
    + change uint -> int
    + ctors initialise dictionary
    + changed Count to return total items in basket or unique items in basket

--- CMP 205 Unit 1 WPF Application for Comic Book Store scenario/DundeeComicBookStore/Models/BasketModel.cs
+++ CMP 205 Unit 1 WPF Application for Comic Book Store scenario/DundeeComicBookStore/Models/BasketModel.cs
@@ -24,12 +24,12 @@ namespace DundeeComicBookStore.Models
         /// <summary>
         /// ACCESS VIA PUBLIC METHOD
         /// </summary>
-        private Dictionary<IProduct, uint> _items;
+        private Dictionary<IProduct, int> _items;
 
         /// <summary>
         /// Method for getting and setting the basket's products and quantities
         /// </summary>
-        public Dictionary<IProduct, uint> Items
+        public Dictionary<IProduct, int> Items
         {
             get { return _items; }
             set { _items = value; }
@@ -39,16 +39,18 @@ namespace DundeeComicBookStore.Models
         /// Create a new instance with the owning user defined
         /// </summary>
         /// <param name="user">An IUser object</param>
+
         public BasketModel(IUser user)
         {
             User = user;
+            Items = new Dictionary<IProduct, int>();
         }
 
         /// <summary>
         /// Create a new instance with the given dictionary of products
         /// </summary>
-        /// <param name="products">Products stored in a dictionary of IProduct,uint</param>
-        public BasketModel(Dictionary<IProduct, uint> products)
+        /// <param name="products">Products stored in a dictionary of IProduct,int</param>
+        public BasketModel(Dictionary<IProduct, int> products)
         {
             Items = products;
         }
@@ -58,19 +60,22 @@ namespace DundeeComicBookStore.Models
         /// </summary>
         /// <param name="user">An IUser object</param>
         /// <param name="product">An IProduct object</param>
-        /// <param name="quantity">The quantity of item to order as uint</param>
-        public BasketModel(IUser user, IProduct product, uint quantity)
+        /// <param name="quantity">The quantity of item to order as int</param>
+        public BasketModel(IUser user, IProduct product, int quantity)
         {
             User = user;
-            Items.Add(product, quantity);
+            Items = new Dictionary<IProduct, int>
+            {
+                { product, quantity }
+            };
         }
 
         /// <summary>
         /// Create a new instance with the given user, and dictionary of products
         /// </summary>
         /// <param name="user">An IUser object</param>
-        /// <param name="products">Products stored in a dictionary of IProduct,uint</param>
-        public BasketModel(IUser user, Dictionary<IProduct, uint> products)
+        /// <param name="products">Products stored in a dictionary of IProduct,int</param>
+        public BasketModel(IUser user, Dictionary<IProduct, int> products)
         {
             User = user;
             Items = products;
@@ -80,27 +85,31 @@ namespace DundeeComicBookStore.Models
         /// Add the given product and quantity to Items
         /// </summary>
         /// <param name="product">An IProduct object</param>
-        /// <param name="quantity">The quantity of item to order as uint</param>
-        public void Add(IProduct product, uint quantity)
+        /// <param name="quantity">The quantity of item to order as int</param>
+        public void Add(IProduct product, int quantity)
         {
-            Items.Add(product, quantity);
+            if (Items.ContainsKey(product)) Items[product] += quantity;
+            else Items.Add(product, quantity);
         }
 
         /// <summary>
         /// Add the given values in the dictionary to Items
         /// </summary>
-        /// <param name="products">Products stored in a dictionary of IProduct,uint</param>
-        public void Add(Dictionary<IProduct, uint> products)
+        /// <param name="products">Products stored in a dictionary of IProduct,int</param>
+        public void Add(Dictionary<IProduct, int> products)
         {
             foreach (var kvp in products)
-                Items.Add(kvp.Key, kvp.Value);
+            {
+                if (Items.ContainsKey(kvp.Key)) Items[kvp.Key] += kvp.Value;
+                else Items.Add(kvp.Key, kvp.Value);
+            }
         }
 
         /// <summary>
         /// Set Items to the given dictionary
         /// </summary>
-        /// <param name="products">Products stored in a dictionary of IProduct,uint</param>
-        public void Set(Dictionary<IProduct, uint> products)
+        /// <param name="products">Products stored in a dictionary of IProduct,int</param>
+        public void Set(Dictionary<IProduct, int> products)
         {
             Items = products;
         }
@@ -110,7 +119,7 @@ namespace DundeeComicBookStore.Models
         /// </summary>
         /// <param name="target">The IProduct object to alter</param>
         /// <param name="value">The value to be set as the new quantity</param>
-        public void SetQuantity(IProduct target, uint value)
+        public void SetQuantity(IProduct target, int value)
         {
             Items[target] = value;
         }
@@ -119,9 +128,14 @@ namespace DundeeComicBookStore.Models
         /// Gets Items.Count
         /// </summary>
         /// <returns>The number of products in the basket</returns>
-        public int Count()
+        public int Count(bool unique = false)
         {
-            return Items.Count;
+            if (unique) return Items.Count;
+
+            int count = 0;
+            foreach (var kvp in Items)
+                count += kvp.Value;
+            return count;
         }
     }
 }
