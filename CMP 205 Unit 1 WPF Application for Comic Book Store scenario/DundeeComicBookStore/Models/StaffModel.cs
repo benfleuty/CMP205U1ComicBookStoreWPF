1aa85c14979ca352d6fc5d0366cabcec41d940f0
Ben Fleuty <22148138+benfleuty@users.noreply.github.com>
2020-12-12 16:50:03 +0000

    add comments to represent bits to permissions

--- CMP 205 Unit 1 WPF Application for Comic Book Store scenario/DundeeComicBookStore/Models/StaffModel.cs
+++ CMP 205 Unit 1 WPF Application for Comic Book Store scenario/DundeeComicBookStore/Models/StaffModel.cs
@@ -19,26 +19,26 @@ namespace DundeeComicBookStore.Models
         [Flags]
         public enum StaffPermissions
         {
-            None = 0,
-            ReadCustomerData = 1,
-            WriteCustomerData = 1 << 1,
-            DeleteCustomerData = 1 << 2,
+            None = 0,                       // 0000 0000
+            ReadCustomerData = 1,           // 0000 0001
+            WriteCustomerData = 1 << 1,     // 0000 0010
+            DeleteCustomerData = 1 << 2,    // 0000 0100
 
-            ReadStockData = 1 << 3,
-            WriteStockData = 1 << 4,
-            DeleteStockData = 1 << 5,
+            ReadStockData = 1 << 3,         // 0000 1000
+            WriteStockData = 1 << 4,        // 0001 0000
+            DeleteStockData = 1 << 5,       // 0010 0000
 
-            AccessEmployeeData = 1 << 6,
+            AccessEmployeeData = 1 << 6,    // 0100 0000
 
-            CanReadAll = ReadCustomerData & ReadStockData,
-            CanWriteAll = WriteCustomerData & WriteStockData,
-            CanDeleteAll = DeleteCustomerData & DeleteStockData,
+            CanReadAll = ReadCustomerData & ReadStockData,          // 0000 1001
+            CanWriteAll = WriteCustomerData & WriteStockData,       // 0001 0010
+            CanDeleteAll = DeleteCustomerData & DeleteStockData,    // 0010 0100
 
-            AllOnCustomerData = ReadCustomerData & WriteCustomerData & DeleteCustomerData,
+            AllOnCustomerData = ReadCustomerData & WriteCustomerData & DeleteCustomerData, // 0000 0111
 
-            AllOnStockData = ReadStockData & WriteStockData & DeleteStockData,
+            AllOnStockData = ReadStockData & WriteStockData & DeleteStockData,             // 0011 1000
 
-            All = AllOnCustomerData & AllOnStockData & AccessEmployeeData
+            All = AllOnCustomerData & AllOnStockData & AccessEmployeeData                  // 0111 1111
         }
 
         public byte Permissions { get; set; }
