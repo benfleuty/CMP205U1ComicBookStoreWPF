f30e35eb1cbe4f2d07f468071e2f974be55ce8a9
Ben Fleuty <22148138+benfleuty@users.noreply.github.com>
2020-12-13 16:06:27 +0000

    Add StaffLoginPage

---
+++ CMP 205 Unit 1 WPF Application for Comic Book Store scenario/DundeeComicBookStore/Pages/StaffLandingPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using DundeeComicBookStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DundeeComicBookStore.Pages
+{
+    /// <summary>
+    /// Interaction logic for StaffLandingPage.xaml
+    /// </summary>
+    public partial class StaffLandingPage : BasePage
+    {
+        private StaffModel _staff;
+
+        public StaffModel Staff
+        {
+            get { return _staff; }
+            set { _staff = value; }
+        }
+
+        public StaffLandingPage(StaffModel staff)
+        {
+            InitializeComponent();
+            Staff = staff;
+        }
+    }
+}
