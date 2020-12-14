cacd6239ad9bc7420f0de359a2915c39b7848fdc
Ben Fleuty <22148138+benfleuty@users.noreply.github.com>
2020-12-14 15:37:17 +0000

    add permission checking

--- CMP 205 Unit 1 WPF Application for Comic Book Store scenario/DundeeComicBookStore/Models/StaffModel.cs
+++ CMP 205 Unit 1 WPF Application for Comic Book Store scenario/DundeeComicBookStore/Models/StaffModel.cs
@@ -1,7 +1,6 @@
 ﻿using DundeeComicBookStore.Interfaces;
 using System;
-using System.Collections.Generic;
-using System.Text;
+using System.Collections;
 
 namespace DundeeComicBookStore.Models
 {
@@ -18,6 +17,21 @@ namespace DundeeComicBookStore.Models
         public string ProfilePictureSource { get; set; }
         public uint RewardPoints { get; set; }
 
+        public enum Permission
+        {
+            // array access is 0-7
+            // perms are stored 7-0
+            All,                // [0] --> 7
+
+            AccessEmployeeData, // [1] --> 6
+            DeleteStockData,    // [2] --> 5
+            WriteStockData,     // [3] --> 4
+            ReadStockData,      // [4] --> 3
+            DeleteCustomerData, // [5] --> 2
+            WriteCustomerData,  // [6] --> 1
+            ReadCustomerData    // [7] --> 0
+        }
+
         [Flags]
         public enum StaffPermissions
         {
@@ -32,21 +46,86 @@ namespace DundeeComicBookStore.Models
 
             AccessEmployeeData = 64,    // 0100 0000 // 0x40
 
-            CanReadAll = ReadCustomerData & ReadStockData,          // 0000 1001 // 0x09 // 9
-            CanWriteAll = WriteCustomerData & WriteStockData,       // 0001 0010 // 0x12 // 18
-            CanDeleteAll = DeleteCustomerData & DeleteStockData,    // 0010 0100 // 0x24 // 36
+            CanReadAll = ReadCustomerData + ReadStockData,          // 0000 1001 // 0x09 // 9
+            CanWriteAll = WriteCustomerData + WriteStockData,       // 0001 0010 // 0x12 // 18
+            CanDeleteAll = DeleteCustomerData + DeleteStockData,    // 0010 0100 // 0x24 // 36
 
             // 0000 0111 // 0x07 // 7
-            AllOnCustomerData = ReadCustomerData & WriteCustomerData & DeleteCustomerData,
+            AllOnCustomerData = ReadCustomerData + WriteCustomerData + DeleteCustomerData,
 
             // 0011 1000 // 0x38 // 56
-            AllOnStockData = ReadStockData & WriteStockData & DeleteStockData,
+            AllOnStockData = ReadStockData + WriteStockData + DeleteStockData,
 
             // 0111 1111 // 0x7F // 127
-            All = AllOnCustomerData & AllOnStockData & AccessEmployeeData
+            All = AllOnCustomerData + AllOnStockData + AccessEmployeeData
         }
 
-        public byte Permissions { get; set; }
+        private BitArray _permissions;
+
+        public BitArray Permissions
+        {
+            get => _permissions;
+            set => _permissions = value;
+        }
+
+        public bool Can(Permission perm)
+        {
+            byte permToCheck = (byte)perm;
+            if (Permissions[permToCheck])
+                return true;
+            else return false;
+        }
+
+        public bool Can(StaffPermissions perm)
+        {
+            bool result;
+
+            switch ((StaffPermissions)Convert.ToByte(perm))
+            {
+                case StaffPermissions.CanReadAll:
+                    result = Can(Permission.ReadCustomerData)
+                        && Can(Permission.ReadStockData);
+                    break;
+
+                case StaffPermissions.CanWriteAll:
+                    result = Can(Permission.WriteCustomerData)
+                        && Can(Permission.WriteStockData);
+                    break;
+
+                case StaffPermissions.CanDeleteAll:
+                    result = Can(Permission.DeleteCustomerData)
+                        && Can(Permission.DeleteStockData);
+                    break;
+
+                case StaffPermissions.AllOnCustomerData:
+                    result = Can(Permission.ReadCustomerData)
+                        && Can(Permission.WriteCustomerData)
+                        && Can(Permission.DeleteCustomerData);
+                    break;
+
+                case StaffPermissions.AllOnStockData:
+                    result = Can(Permission.ReadStockData)
+                        && Can(Permission.WriteStockData)
+                        && Can(Permission.DeleteStockData);
+                    break;
+
+                case StaffPermissions.All:
+                    result = Can(StaffPermissions.AllOnCustomerData)
+                        && Can(StaffPermissions.AllOnStockData)
+                        && Can(Permission.AccessEmployeeData);
+                    break;
+
+                default:
+                    throw new Exception("Permission not matched!");
+            }
+
+            return result;
+
+            //byte permToCheck = (byte)perm;
+            //if (Permissions[permToCheck])
+            //    return true;
+            //else return false;
+        }
 
         public bool IsStaff => true;
     }
