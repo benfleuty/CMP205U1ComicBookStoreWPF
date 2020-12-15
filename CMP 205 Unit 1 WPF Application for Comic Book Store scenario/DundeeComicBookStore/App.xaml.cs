3af4defa9a687a66f21ff909a6892fe3404c5828
Ben Fleuty <22148138+benfleuty@users.noreply.github.com>
2020-12-15 23:38:33 +0000

    re add app

---
+++ CMP 205 Unit 1 WPF Application for Comic Book Store scenario/DundeeComicBookStore/App.xaml.cs
@@ -0,0 +1,47 @@
+﻿using DundeeComicBookStore.Interfaces;
+using DundeeComicBookStore.Models;
+using DundeeComicBookStore.Pages;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace DundeeComicBookStore
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            //string debugEmail = "bfleuty@outlook.com";
+            string debugEmail = "bfleuty@outlook.com";
+            string debugPassword = "123456";
+            IUser user = DBAccessHelper.GetUser(debugEmail, debugPassword);
+
+            MainWindow window = new MainWindow();
+            if (user == null)
+            {
+                RegisterPage page = new RegisterPage();
+                window.MainFrame.Content = page;
+            }
+            else
+            {
+                //OrderModel om = new OrderModel()
+                //{
+                //    User = user
+                //};
+                var staff = (StaffModel)user;
+                var page = new StaffLandingPage(staff);
+                window.MainFrame.Content = page;
+            }
+
+            window.Show();
+        }
+    }
+}
