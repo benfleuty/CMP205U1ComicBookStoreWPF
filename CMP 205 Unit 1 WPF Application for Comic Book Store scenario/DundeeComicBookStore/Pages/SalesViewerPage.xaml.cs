3421a30ad24bfc51d6b578e72e6539b629cd8b31
Ben Fleuty <22148138+benfleuty@users.noreply.github.com>
2020-12-15 19:37:40 +0000

    Sales Viewer created

--- CMP 205 Unit 1 WPF Application for Comic Book Store scenario/DundeeComicBookStore/Pages/SalesViewerPage.xaml.cs
+++ CMP 205 Unit 1 WPF Application for Comic Book Store scenario/DundeeComicBookStore/Pages/SalesViewerPage.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using DundeeComicBookStore.Models;
 using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -31,6 +33,148 @@ namespace DundeeComicBookStore.Pages
         {
             InitializeComponent();
             Staff = staff;
+            GetData();
+        }
+
+        private void GetData()
+        {
+            using SqlConnection conn = new SqlConnection(ConnectionHelper.ConnVal("mssql1900040"));
+            try
+            {
+                conn.Open();
+
+                #region Get number of items ordered
+
+                var sql = new StringBuilder();
+                sql.Append("SELECT SUM(quantity) as quantity ");
+                sql.Append("FROM OrderItems ");
+
+                string query = sql.ToString();
+
+                SqlCommand command = new SqlCommand(query, conn);
+
+                SqlDataReader reader = command.ExecuteReader();
+                DataTable dataTable = new DataTable();
+                dataTable.Load(reader);
+
+                int totalItems = (int)dataTable.Rows[0]["quantity"];
+
+                #endregion Get number of items ordered
+
+                #region Get number of orders placed
+
+                sql = new StringBuilder();
+                sql.Append("SELECT COUNT(id) as id ");
+                sql.Append("FROM Orders ");
+
+                query = sql.ToString();
+
+                command = new SqlCommand(query, conn);
+
+                reader = command.ExecuteReader();
+                dataTable = new DataTable();
+                dataTable.Load(reader);
+
+                int totalOrders = (int)dataTable.Rows[0]["id"];
+
+                #endregion Get number of orders placed
+
+                #region Get gross revenue
+
+                sql = new StringBuilder();
+                sql.Append("SELECT SUM(amount) as amount ");
+                sql.Append("FROM Payments ");
+
+                query = sql.ToString();
+
+                command = new SqlCommand(query, conn);
+
+                reader = command.ExecuteReader();
+                dataTable = new DataTable();
+                dataTable.Load(reader);
+
+                decimal grossRevenue = (decimal)dataTable.Rows[0]["amount"];
+
+                #endregion Get gross revenue
+
+                #region Get expenses
+
+                sql = new StringBuilder();
+                sql.Append("SELECT productId,quantity ");
+                sql.Append("FROM OrderItems,Payments ");
+                sql.Append("WHERE OrderItems.orderId = Payments.orderId ");
+
+                query = sql.ToString();
+
+                command = new SqlCommand(query, conn);
+
+                reader = command.ExecuteReader();
+                dataTable = new DataTable();
+                dataTable.Load(reader);
+
+                var productsSold = new Dictionary<int, int>();
+
+                foreach (DataRow row in dataTable.Rows)
+                {
+                    int productId = (int)row["productId"];
+                    int quantity = (int)row["quantity"];
+                    if (productsSold.ContainsKey(productId))
+                        productsSold[productId] += quantity;
+                    else productsSold[productId] = quantity;
+                }
+
+                decimal totalExpense = 0.0m;
+
+                foreach (var product in productsSold)
+                {
+                    sql = new StringBuilder();
+                    sql.Append("SELECT unitCost ");
+                    sql.Append("FROM Products ");
+                    sql.Append($"WHERE id = {product.Key}");
+
+                    query = sql.ToString();
+
+                    command = new SqlCommand(query, conn);
+
+                    reader = command.ExecuteReader();
+                    dataTable = new DataTable();
+                    dataTable.Load(reader);
+
+                    totalExpense += decimal.Parse(dataTable.Rows[0]["unitCost"].ToString()) * product.Value;
+                }
+
+                #endregion Get expenses
+
+                #region Net revenue
+
+                decimal netRevenue = grossRevenue - totalExpense;
+
+                #endregion Net revenue
+
+                numOfItemsSold.Text = totalItems.ToString();
+
+                numOfOrders.Text = totalOrders.ToString();
+
+                grossRevenueTb.Text = grossRevenue.ToString("C");
+
+                expenses.Text = totalExpense.ToString("C");
+
+                netRevenueTb.Text = netRevenue.ToString("C");
+            }
+            catch (Exception e)
+            {
+                return;
+            }
+        }
+
+        private void LogoutButton_Click(object sender, RoutedEventArgs e)
+        {
+            ChangePageTo(new LoginPage());
+        }
+
+        private void HomeButton_Click(object sender, RoutedEventArgs e)
+        {
+            ChangePageTo(new StaffLandingPage(Staff));
         }
     }
 }
