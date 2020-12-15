db3a45e152bd6924a8859f0ed107b6fd1c84dc3f
Ben Fleuty <22148138+benfleuty@users.noreply.github.com>
2020-12-15 01:15:24 +0000

    add / remove all permissions buttons added

--- CMP 205 Unit 1 WPF Application for Comic Book Store scenario/DundeeComicBookStore/Pages/EntityEditorPage.xaml.cs
+++ CMP 205 Unit 1 WPF Application for Comic Book Store scenario/DundeeComicBookStore/Pages/EntityEditorPage.xaml.cs
@@ -244,9 +244,127 @@ ORDER BY Orders.id DESC";
 
         #endregion Top bar events
 
+        #region DataGrid
+
+        private void ParseTableToVars(DataRow row)
+        {
+            if (Entity == EntityType.CustomerRecord)
+                ParseTableToCustomerForm(row);
+            else if (Entity == EntityType.ProductRecord)
+                ParseTableToProductForm(row);
+            else if (Entity == EntityType.OrderRecord)
+                ParseTableToOrderForm(row);
+            else if (Entity == EntityType.StaffRecord)
+                ParseTableToEmployeeForm(row);
+        }
+
+        private void ParseTableToCustomerForm(DataRow row)
+        {
+            formCustomerFirstNameTextbox.Text = (string)row["firstName"];
+            formCustomerLastNameTextbox.Text = (string)row["lastName"];
+            formCustomerPhoneNumberTextbox.Text = (string)row["phone"];
+            formCustomerEmailAddressTextbox.Text = (string)row["email"];
+            string content = (string)row["address"];
+            string[] address = content.Split('|');
+            formCustomerHouseNumberNameTextbox.Text = address[0];
+            formCustomerPostCodeTextbox.Text = address[1];
+        }
+
+        private void ParseTableToProductForm(DataRow row)
+        {
+            formCustomerFirstNameTextbox.Text = (string)row["firstName"];
+            formCustomerLastNameTextbox.Text = (string)row["lastName"];
+            formCustomerPhoneNumberTextbox.Text = (string)row["phone"];
+            formCustomerEmailAddressTextbox.Text = (string)row["email"];
+            string content = (string)row["address"];
+            string[] address = content.Split('|');
+            formCustomerHouseNumberNameTextbox.Text = address[0];
+            formCustomerPostCodeTextbox.Text = address[1];
+        }
+
+        private void ParseTableToOrderForm(DataRow row)
+        {
+            formCustomerFirstNameTextbox.Text = (string)row["firstName"];
+            formCustomerLastNameTextbox.Text = (string)row["lastName"];
+            formCustomerPhoneNumberTextbox.Text = (string)row["phone"];
+            formCustomerEmailAddressTextbox.Text = (string)row["email"];
+            string content = (string)row["address"];
+            string[] address = content.Split('|');
+            formCustomerHouseNumberNameTextbox.Text = address[0];
+            formCustomerPostCodeTextbox.Text = address[1];
+        }
+
+        private void ParseTableToEmployeeForm(DataRow row)
+        {
+            formEmployeeFirstNameTextbox.Text = (string)row["firstName"];
+            formEmployeeLastNameTextbox.Text = (string)row["lastName"];
+            formEmployeePhoneNumberTextbox.Text = (string)row["phone"];
+            formEmployeeEmailAddressTextbox.Text = (string)row["email"];
+            string content = (string)row["address"];
+            string[] address = content.Split('|');
+            formEmployeeHouseNumberNameTextbox.Text = address[0];
+            formEmployeePostCodeTextbox.Text = address[1];
+
+            byte permissions = ((byte[])row["permissions"])[0];
+
+            StaffModel selected = new StaffModel()
+            {
+                Permissions = DBAccessHelper.CalculatePermissions(permissions)
+            };
+
+            if (selected.Can(StaffModel.Permission.ReadCustomerData))
+                employeeCB_RCD.IsChecked = true;
+
+            if (selected.Can(StaffModel.Permission.WriteCustomerData))
+                employeeCB_WCD.IsChecked = true;
+
+            if (selected.Can(StaffModel.Permission.DeleteCustomerData))
+                employeeCB_DCD.IsChecked = true;
+
+            if (selected.Can(StaffModel.Permission.ReadStockData))
+                employeeCB_RSD.IsChecked = true;
+
+            if (selected.Can(StaffModel.Permission.WriteStockData))
+                employeeCB_WSD.IsChecked = true;
+
+            if (selected.Can(StaffModel.Permission.DeleteStockData))
+                employeeCB_DSD.IsChecked = true;
+
+            if (selected.Can(StaffModel.Permission.AccessEmployeeData))
+                employeeCB_AED.IsChecked = true;
+        }
+
+        #region Events
+
+        private void ResultDg_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var sent = sender as DataGrid;
+            DataRowView drv = (DataRowView)sent.SelectedItem;
+            DataRow row = drv.Row;
+            ParseTableToVars(row);
+        }
+
+        #endregion Events
+
+        #endregion DataGrid
+
         #region Form
 
         #region Form events
+
+        private void EmployeeBtn_applyAll_Click(object sender, RoutedEventArgs e)
+        {
+            employeeCB_RCD.IsChecked = employeeCB_WCD.IsChecked = employeeCB_DCD.IsChecked =
+                employeeCB_RSD.IsChecked = employeeCB_WSD.IsChecked = employeeCB_DSD.IsChecked =
+                employeeCB_AED.IsChecked = true;
+        }
+
+        private void EmployeeBtn_removeAll_Click(object sender, RoutedEventArgs e)
+        {
+            employeeCB_RCD.IsChecked = employeeCB_WCD.IsChecked = employeeCB_DCD.IsChecked =
+                employeeCB_RSD.IsChecked = employeeCB_WSD.IsChecked = employeeCB_DSD.IsChecked =
+                employeeCB_AED.IsChecked = false;
+        }
 
         private void SaveChanges_Click(object sender, RoutedEventArgs e)
         {
